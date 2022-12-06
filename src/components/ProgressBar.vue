194a6211c56e3a599f50eb905eec23d33d406ebc
Archit Jain <32292295+architjee@users.noreply.github.com>
2022-12-06 13:12:46 +0530

    Skill Progress bars working

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="single-progress">
+        <div class="is-size-5">
+            {{ label }}
+        </div>
+        <progress class="progress" :value="size" max="100" :class="color">{{size}}</progress>
+    </div>
+</template>
+  
+<script>
+export default {
+    props: ['label','size','color'],
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+    mounted() {
+
+    }
+}
+</script>
--- src/views/SkillsPage.vue
+++ src/views/SkillsPage.vue
@@ -1,8 +1,49 @@
 <script >
-
+import ProgressBar from '../components/ProgressBar.vue';
+export default {
+    components: {
+        'my-progress-bar': ProgressBar,
+    }
+}
 </script>
 
 <template>
-SKILLS
+  
+       <div class="container">
+           <div class="columns">
+               <div class="column is-half">
+                   <div class="card">
+                       <header class="card-header">
+                           <p class="card-header-title">
+                               Programming Languages
+                           </p>
+                       </header>
+                       <div class="card-content">
+                           <div class="content">
+                            <p>
+                                <my-progress-bar label="Python" size="80" color="is-info"></my-progress-bar>
+
+                            </p>
+                          <p>
+                              <my-progress-bar label="JavaScript" size="80" color="is-info"></my-progress-bar>
+
+                          </p>
+                          <p>
+                              <my-progress-bar label="Java" size="80" color="is-info"></my-progress-bar>
+
+                          </p>
+                          <p>
+                              <my-progress-bar label="C/C++" size="80" color="is-info"></my-progress-bar>
+
+                          </p>
+                           </div>
+                       </div>
+                   </div>
+   
+               </div>
+           </div>
+       </div>
+
+
 
 </template>
